9e7c91c883cee461639e42a8c3c851adbc265c27
addy1397 <adityasingh1397@gmail.com>
2018-10-02 03:48:11 +0530

    Models added

--- airportManagementSystem/airportManagementSystem/settings.py
+++ airportManagementSystem/airportManagementSystem/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ticketing.apps.TicketingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ airportManagementSystem/ticketing/models.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Person(models.Model):
+    name = models.CharField(max_length=40)
+    address = models.CharField(max_length=50)
+    nationality = models.CharField(max_length=30)
+    iD = models.CharField(primary_key=True, max_length=20)
+    creditCard = models.CharField(max_length=20)
+
+class Ticket(models.Model):
+    startingPoint = models.CharField(max_length=20)
+    destination = models.CharField(max_length=20)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    time = models.CharField(max_length=10)
+    price = models.IntegerField(default = 1000)
